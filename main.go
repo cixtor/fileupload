70f0d6a06038d640cfa5094950656bf488a35380
Cixtor <cixtords@gmail.com>
2021-08-20 10:34:28 -0800

    Add function to copy data from a source to the local disk

--- main.go
+++ main.go
@@ -3,17 +3,27 @@ package main
 import (
 	_ "embed"
 
+	"flag"
+	"fmt"
 	"html/template"
+	"io"
 	"log"
+	"mime/multipart"
 	"net/http"
+	"os"
 
 	"github.com/cixtor/middleware"
 )
+
+var directory string
 
 //go:embed index.html
 var htmlIndex string
 
 func main() {
+	flag.StringVar(&directory, "d", ".", "Destination folder to store the uploaded files")
+	flag.Parse()
+
 	srv := middleware.New()
 	srv.GET("/", index)
 	log.Fatal(srv.ListenAndServe(":3000"))
@@ -29,4 +39,28 @@ func index(w http.ResponseWriter, r *http.Request) {
 	if err := tpl.Execute(w, nil); err != nil {
 		panic(err)
 	}
+}
+
+func uploadThisFile(fileHeader *multipart.FileHeader) error {
+	file, err := fileHeader.Open()
+
+	if err != nil {
+		return fmt.Errorf("uploadThisFile %w", err)
+	}
+
+	defer file.Close()
+
+	dst, err := os.Create(directory + "/" + fileHeader.Filename)
+
+	if err != nil {
+		return fmt.Errorf("uploadThisFile %w", err)
+	}
+
+	defer dst.Close()
+
+	if _, err := io.Copy(dst, file); err != nil {
+		return fmt.Errorf("uploadThisFile %w", err)
+	}
+
+	return nil
 }
